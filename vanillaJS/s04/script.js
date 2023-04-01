bf5e4cddb2c920a156f5379b689817bdfe090a54
Jiezel Olandag <agum.jhay@gmail.com>
2023-04-01 11:47:11 +0800

    save

--- vanillaJS/s04/script.js
+++ vanillaJS/s04/script.js
@@ -1,11 +1,11 @@
-console.log ("connected");
+console.log("connected");
 
-//for Loops - repeats a block of coede as long as a certain condition is met.
-// it is used to execute a block of code for certain numberof times.
+//For Loops - repeats a block of code as long as a certain condition is met.
+// it is used to execute a block of code for certain number of times.
 
 /*
     SYNTAX:
-    for (initializing; condition; icrement) - these three are what we call params (parameters)
+    for (initialization; condition; increment) - these three are what we call params(parameters)
     {
         //block of code
     }
@@ -14,90 +14,137 @@ console.log ("connected");
 
     2. condition - evaluated at the beginning of each iteration.
         - if true, the loop statements execute
-        -if false, execution of the loop ends
+        - if false, execution of the loop ends
     
     3. increment - updates the loop counter with a new value each time the loop runs.
 */
 
-for (let i = 0; i <= 5; i++)
-{
-    console.log(`This is number ${i}`);
-}
+// for (let i = 0; i <= 5; i++)
+// {
+//     console.log (`This is number ${i}`);
+// }
 
-//if the condition is <= 5 it displays the output from 0 to 5
-//if the condition is < 5 it will display the output from 0 to 4
+// //if the condition is <= 5 it displays the output from 0 to 5
+// //if the condition is < 5 it will display the output from 0 to 4
 
-let herName = "Lady Ada Augusta Byron King";
-console.log(herName.length);
-console.log(herName[6]); //d
-console.log(herName[13]); //s
+// let herName = "Lady Ada Augusta Byron King";
+// console.log (herName.length); //27
+// console.log (herName[6]); //d
+// console.log (herName[13]);//s
 
-let letter = 0;
+// let letter = 0;
 
-for (letter; letter<herName.length; letter++)
-{
-    console.log(herName[letter]);
-}
+// for (letter; letter<herName.length; letter++) 
+// {
+//     console.log(herName[letter])
+// }
 
-let fruits = ["guava", "mangosteen", "santol", "durian"]
-console.log(fruits);
-fruits [4] = "banana";
-fruits [5] = "strawberry";
-console.log(fruits.length); //6
+// let fruits = ["guava", "mangosteen", "santol", "durian"];
+// console.log (fruits);
+// fruits [4] = "banana";
+// fruits [5] = "strawberry";
+// console.log(fruits.length); //6
 
-let index = 0;
-for (index; index<fruits.length; index++)
-{
-    console.log(fruits[index]);
-}
+// let index = 0;
+// for (index; index<fruits.length; index++)
+// {
+//     console.log(fruits[index])
+// }
 
-let cars = [
-    {
-        brand: "Volkswagen",
-        type: "SUV"
-    },
+// let cars = [
+//     {
+//         brand: "Volkswagen",
+//         type: "SUV"
+//     },
 
-    {
-        brand: "Toyota",
-        type: "Sedan"
-    },
+//     {
+//         brand: "Toyota",
+//         type: "Sedan"
+//     },
 
-    {
-        brand: "Rolls Royce",
-        type: "Phantom"
-    }
-];
+//     {
+//         brand: "Rolls Royce",
+//         type: "Phantom"
+//     }
+// ];
 
-let x = 0;
+// let x = 0;
 
-for (x; x<cars.length; x++)
-{
-    console.log(cars[x]); //it dispalys the object properties such as brand and its type
-    console.log(cars[x].brand); //brand printed
-    console.log(cars[x].type); //it displays the property called types
-}
+// for (x; x<cars.length; x++)
+// {
+//     // console.log(cars[x]); //it displays the object properties such as brand and its type
+//     // console.log(cars[x].brand); //brand printed
+//     console.log (cars[x].type); //it displays the property called types
+// }
 
-//whie loop starts by evaluating condition
-//if true, code block gets executed.
+//while loop starts by evaluating condition
+//if true, code block gets executed
 //if false, code block not executed and it terminates the loop
 
 /*
-    SYNTAX:
+    SYNTAX: 
     while (condition)
     {
         //statement
+        //increment
     }
 */
 
-let count = 0;
-while (count <=5) {
-    console.log(count);
-    count++
-}
+// let count = 0;
+// while (count <= 5) {
+//     console.log(count)
+//     count++;
+// }
 
-let i = 0;
-while (i <= 6)
+// let i = 2;
+// while (i <= 6)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+//Do-While Loop
+//same as while loop
+//condition is checked at the end of each iteration of the loop,rather than at the beginning before the loop runs.
+
+/*
+    SYNTAX: 
+
+    do {
+        //statement
+        //increment
+    } while (condition)
+
+*/
+
+// let count = 0;
+
+// do {
+
+//     console.log (count)
+//     count++;
+
+// } while (count <= 10)
+
+//BONUS
+//continue statement - breaks one iteration (in the loop) if a specified condition occurs
+//continues with the next iteration of the loop
+//jumps over one iteration in the loop
+
+//skipping numbers using for loop
+let text = "";
+// let j = 1;
+
+// for (j;j<8; j++ )
+// {
+//     if (j === 2 || j === 3) continue;
+//     console.log(text += j)
+// }
+
+const car = ["BMW", "Volvo", "Saab", "Ford"];
+
+for (let ai = 0; ai < car.length; ai++)
 {
-    console.log(i);
-    i++
+    if (car[ai] === "Saab") continue;
+    console.log(text += car[ai])
 }
---
+++ vanillaJS/s05/script.js
@@ -0,0 +1,224 @@
+console.log("connected");
+
+//Nested If
+// function oddOrEven (num) {
+//     if (isNaN (num) || typeof parseInt(num) !=="number") 
+//     //determines whether a value is Nan (Not a number) or a number
+//     {
+//         return `Unexpected Input`;
+//     }
+//     else if (num == ``){
+//         return "Please input inside the box"
+//     }
+//     else {
+//         if (num % 2 == 0)
+//         {
+//             return ("even number")
+//         }
+//         else {
+//             return ("odd number")
+//         }
+//     }
+// }
+
+// let userInput = prompt  ("Give me a number");
+// console.log(oddOrEven(userInput));
+
+// Nested Loops
+// it composed of outer loop and inner loop
+
+/*
+    SYNTAX:
+    // outer loop
+    for (init; condition; increment/decrement)
+    {
+        // statement
+        // inner loop
+        for (init; condition; increment/decrement){
+            // statement of inner loop
+        }
+
+        // statement
+    }
+*/
+
+// for (let i = 0; i <= 2 ; i++) {
+//     console.log("-outer loop");
+
+//     for (let j = 0; j <= 3; j++)
+//     {
+//         console.log("--inner loop");
+//     }
+// }
+
+// let i = 0;
+// while (i <= 2) {
+//     console.log("-outer loop");
+//     i++;
+
+//     let j=0;
+//     while (j <=3)
+//     {
+//         console.log("--inner loop");
+//         j++;
+//     }
+// }
+
+// function multiplyAll (arr)
+// {
+//     let product = 1;
+
+//     for (let i=0; i <arr.length; i++)
+//     {
+//         for (let j=0; j < arr[i].length; j++)
+//         {
+//             product *= arr[i][j];
+//         }
+//     }
+
+//     console.log(product);
+//     return product
+// }
+
+// // invoke the function
+// multiplyAll([[2,5],[9,10],[11,17,20]]);
+
+// SOLUTION
+const name = ["Karen", "Marites", "Marisol"];
+
+//outer loop
+for (let i = 0; i <name.length; i++)
+{
+    console.log(`My name is ${name[i]}`);
+    
+    for (let j = 0; j <name[i].length; j++)
+    {
+        console.log(`spells ${name[i][j]}`);
+    }
+}
+
+// let i = 0;
+// while (i <= 0) {
+//     console.log("My name is Jiezel");
+//     i++;
+
+//     let j=0;
+//     while (j <=0)
+//     {
+//         console.log("Spell J");
+//         j++;
+//     }
+
+//     {
+//         console.log("Spell I");
+//         j++;
+//     }
+
+//     {
+//         console.log("Spell E");
+//         j++;
+//     }
+
+//     {
+//         console.log("Spell Z");
+//         j++;
+//     }
+
+//     {
+//         console.log("Spell E");
+//         j++;
+//     }
+
+//     {
+//         console.log("Spell L");
+//         j++;
+//     }
+// }
+
+// let l = 0;
+// while (l <= 0) {
+//     console.log("My name is Marites");
+//     l++;
+
+//     let s=0;
+//     while (s <=0)
+
+//     {
+//         console.log("Spell M");
+//         s++;
+//     }
+
+//     {
+//         console.log("Spell A");
+//         s++;
+//     }
+
+//     {
+//         console.log("Spell R");
+//         s++;
+//     }
+
+//     {
+//         console.log("Spell I");
+//         s++;
+//     }
+
+//     {
+//         console.log("Spell T");
+//         s++;
+//     }
+
+//     {
+//         console.log("Spell E");
+//         s++;
+//     }
+
+//     {
+//         console.log("Spell S");
+//         s++;
+//     }
+// }
+
+// let a = 0;
+// while (a <= 0) {
+//     console.log("My name is Marisol");
+//     a++;
+
+//     let f=0;
+//     while (f <=0)
+
+//     {
+//         console.log("Spell M");
+//         f++;
+//     }
+
+//     {
+//         console.log("Spell A");
+//         f++;
+//     }
+
+//     {
+//         console.log("Spell R");
+//         f++;
+//     }
+
+//     {
+//         console.log("Spell I");
+//         f++;
+//     }
+
+//     {
+//         console.log("Spell S");
+//         f++;
+//     }
+
+//     {
+//         console.log("Spell O");
+//         f++;
+//     }
+
+//     {
+//         console.log("Spell L");
+//         f++;
+//     }
+// }
