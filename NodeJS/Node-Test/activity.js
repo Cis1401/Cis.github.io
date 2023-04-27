2e974085eb395ad1559d8b9354c321c39f687d5b
Jiezel Olandag <agum.jhay@gmail.com>
2023-04-27 21:43:35 +0800

    assignment

---
+++ NodeJS/Node-Test/activity.js
@@ -0,0 +1,20 @@
+/*
+    - What directive is used by Node.js in loading the modules it needs?
+    - What Node.js module contains a method for server creation?
+    - What is the method of the http object responsible for creating a server using Node.js?
+    - What method of the response object allows us to set status codes and content types?
+    - Where will console.log() output its contents when run in Node.js?
+    - What property of the request object contains the address's endpoint?
+*/
+
+// 1. Node js used `require()` method for declaring the module/s needed for the system.
+
+// 2. The HTTP module was used to initiate the server creation
+
+// 3. The used method by http is the createServer() method/function for creating the server
+
+// 4. The used method was writeHead() to set the status codes and content type.
+
+// 5. If you used console log in node js it would output into the terminal.
+
+// 6. The request.url is the one responsible of containing or holding the address endpoint.
